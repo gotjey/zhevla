df5a3d49fbf4bb4f3463a4e1c070edf9e050b95a
gotjey <vldslv.ignatenko@gmail.com>
2023-02-20 12:51:19 +0200

    123

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/nikita')
 def index():
-    return 'Hello world!'
+    return 'Hello Nikita!'
 
 if __name__ == '__main__':
     app.run()
